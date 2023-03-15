7d5dfa5cb859542b88b9e5252a0ff6fab1593479
mikefey <mikejfey@hotmail.com>
2023-03-15 09:59:49 -0500

    commit 1

--- src/javaProgram.java
+++ src/javaProgram.java
@@ -1,5 +1,6 @@
 import java.time.Duration;
 import java.util.*;
+import java.util.Random;
 
 public class javaProgram {
     public static void main(String[] args) {
@@ -10,5 +11,27 @@ public class javaProgram {
         System.out.printf("hello %s %d%n", name, age);
         System.out.printf(String.format("%d %S %d%n", age, name, age));
         System.out.println(name);
+        javaProgram.method2(null);
+        javaProgram.randomNumGen(5);
+    }
+    public static void method2(String[] args) {
+        System.out.println("hello mike");
+        String name = "Mike Fey rocks2";
+        int age = 50;
+        System.out.println(name);
+        System.out.println("++++++++++++++");
+        System.out.printf("hello %s %d%n", name, age);
+        System.out.printf(String.format("%d %S %d%n", age, name, age));
+        System.out.println(name);
+    }
+    public static void randomNumGen(int passIn) {
+
+        int num = passIn + 200;
+        System.out.println();
+        System.out.println(num);
+        Random randomNum = new Random();
+        int thisNum = randomNum.nextInt(1000);
+        System.out.println(thisNum);
+
     }
 }
