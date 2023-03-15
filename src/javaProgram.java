06c29ec8d0af5dde87ac768011c33bbe8a1cb9d0
mikefey <mikejfey@hotmail.com>
2023-03-15 10:52:01 -0500

    updated

--- src/javaProgram.java
+++ src/javaProgram.java
@@ -30,9 +30,14 @@ public class javaProgram {
         System.out.println();
         System.out.println(num);
         Random randomNum = new Random();
-        int thisNum = randomNum.nextInt(1000);
+        int thisNum = randomNum.nextInt(10);
+        int thisNum2 = randomNum.nextInt(10);
+        int thisNum3 = randomNum.nextInt(10);
         System.out.println(thisNum);
-
+        System.out.println(thisNum2);
+        System.out.println(thisNum3);
+        String result = String.valueOf(thisNum + "" + thisNum2 + "" + thisNum3);
+        System.out.println(result);
 
     }
 }
